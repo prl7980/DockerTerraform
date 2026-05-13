2067929a6d67b0ac46ff357cb40adc74dcfad076
Patrick Lynch <patrick.r.lynch@hotmail.co.uk>
2026-05-12 21:29:16 -0700

    Add action

--- docker-sqlserver/main.tf
+++ docker-sqlserver/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     docker = {
-      source = "kreuzwerker/docker"
+      source  = "kreuzwerker/docker"
       version = "~> 4.3.0"
     }
   }
@@ -9,21 +9,29 @@ terraform {
 
 provider "docker" {}
 
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
 resource "docker_image" "sqlserver" {
-  name         = "mcr.microsoft.com/mssql/server:2022-latest"
+  name         = "mcr.microsoft.com/mssql/server:2025-latest"
   keep_locally = false
 }
 
 resource "docker_container" "sqlserver" {
-  image = docker_image.sqlserver.image_id
-  name  = "sqlserver"
+  image    = docker_image.sqlserver.image_id
+  name     = "sqlserver"
   hostname = "sqlserver"
+
   ports {
     internal = 1433
     external = 1433
   }
-   env = [
+
+  env = [
     "ACCEPT_EULA=Y",
-    "MSSQL_SA_PASSWORD=P@ssw0rd123",
+    "MSSQL_SA_PASSWORD=${var.db_password}",
   ]
 }
+
